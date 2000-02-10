b9d95cd15ececaefaac677cd388202995706162c
Danny Brian <dbrian@cpan.org>
2000-02-10 00:20:52 +0000

    Import of DBRIAN/Lingua-Wordnet0.1 from CPAN.
    gitpan-cpan-distribution: Lingua-Wordnet
    gitpan-cpan-version:      0.1
    gitpan-cpan-path:         DBRIAN/Lingua-Wordnet0.1.tar.gz
    gitpan-cpan-author:       DBRIAN
    gitpan-cpan-maturity:     released

--- test.pl
+++ test.pl
@@ -1,26 +1,48 @@
 # Before `make install' is performed this script should be runnable with
 # `make test'. After `make install' it should work as `perl test.pl'
 
-######################### We start with some black magic to print on failure.
-
-# Change 1..1 below to 1..last_test_to_print .
-# (It may become useful if the test is moved to ./t subdirectory.)
-
-BEGIN { $| = 1; print "1..1\n"; }
+BEGIN { $| = 1; print "1..6\n"; }
 END {print "not ok 1\n" unless $loaded;}
-use Lingua::Wordnet qw(do_findtheinfo);
+use Lingua::Wordnet;
 $loaded = 1;
 print "ok 1\n";
 
-######################### End of black magic.
 
-# Insert your test code below (better if it prints "ok 13"
-# (correspondingly "not ok 13") depending on the success of chunk 13
-# of the test code):
+$wn = new Lingua::Wordnet;
 
-$word="canary";
-if (do_findtheinfo($word,1,29,0) =~ /canary/) { print "ok 2\n"; }
-    else { print "not ok 2\n"; }
-if (do_findtheinfo($word,1,3,4) =~ /canary/) { print "ok 3\n"; }
-    else { print "not ok 3\n"; }
+my $synset = $wn->lookup_synset_offset("00300911%n");
+
+if ($synset) { print "ok 2\n"; }
+else         { print "not ok 2\n"; }
+
+my $words;
+foreach $bb_synset ($synset->hyponyms) {
+   $bb_synset->add_words("ballser");
+   foreach $word ($bb_synset->words) {
+      $words .= "$word, ";
+   }
+}
+if ($words =~ /hardball/) {
+    print "ok 3\n";
+} else {
+    print "not ok 3\n";
+}
+
+$words = "";
+$synset2 = $wn->lookup_synset("travel","v",2);
+foreach $word ($synset2->words) {
+    $words .= "$word, ";
+}
+$words =~ s/\,\s*$//;
+if ($words =~ /journey/) { print "ok 4\n"; }
+else                     { print "not ok 4\n"; }
+
+if ($wn->familiarity("boy","n") == 4) { print "ok 5\n"; }
+else                                  { print "not ok 5\n"; }
+
+if ($wn->morph("bluest") eq "blue") { print "ok 6\n"; }
+else                                { print "not ok 6\n"; }
+
+$wn->close();
+
 
